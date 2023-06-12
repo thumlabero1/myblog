182ed2ba3ba4cbd3135a2130228940a86d035d84
Phan_Linh <nhomtoanhsg20172018@gmail.com>
2023-06-12 22:09:33 +0700

    main

--- blog/src/components/Navbar/Navbar.js
+++ blog/src/components/Navbar/Navbar.js
@@ -5,18 +5,21 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav>
-      <div className="social-links">
-        <a href="https://facebook.com"><i className="fas fa-facebook">gg</i></a>
-        <a href="https://twitter.com"><i className="fas fa-twitter">gg</i></a>
-        <a href="https://instagram.com"><i className="fas fa-instagram">gg</i></a>
+       <div className="social-links">
+        <a href="https://facebook.com"><i className="fa fa-facebook"></i></a>
+        <a href="https://twitter.com"><i className="fa fa-twitter"></i></a>
+        <a href="https://instagram.com"><i className="fa fa-instagram"></i></a>
       </div>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/page">Page</a></li>   
-        <li><a href="/service">Service</a></li>
-        <li><a href="/shop">Shop</a></li>
-        <li><a href="/blog">Blog</a></li>
-      </ul>
+      <div className="top-nav">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/page">Page</a></li>   
+          <li><a href="/service">Service</a></li>
+          <li><a href="/shop">Shop</a></li>
+          <li><a href="/blog">Blog</a></li>
+        </ul>
+      </div>
+     
     </nav>
   );
 }
